73402ee72ec03c8f7dbad66df949747da51b5bdb
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2022-11-12 19:25:56 +0200

    2

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const AppContainer = styled.div`
-font-size: 18px ;
-color: #010101;
-padding: 20px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
 `
